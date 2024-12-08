fcab38979b522e8948d354daa148b3edf1be49d7
hatnim <dlgotsla3391@gmail.com>
2024-12-08 17:02:37 +0900

    design: 로그인 페이지 UI 추가

--- src/page/LoginPage.jsx
+++ src/page/LoginPage.jsx
@@ -1,4 +1,46 @@
-
-export const LoginPage = ()=> {
-    
-}
+import InputBox from '../component/Input';
+import kakaotalkLoginButton from '../assets/kakaotalk_login_button.png';
+import naverLoginButton from '../assets/naver_login_button.png';
+import googleLoginButton from '../assets/google_login_button.png';
+export const LoginPage = () => {
+  return (
+    <div className="flex flex-col border boder-[#E6E6E6] border-solid rounded-3xl w-2/5 h-4/5 justify-around items-center p-5">
+      <form action="" className="flex flex-col w-4/6 gap-7">
+        <h2 className="text-4xl font-bold text-center">로그인</h2>
+        <label>
+          아이디
+          <InputBox size="default" />
+        </label>
+        <label>
+          비밀번호
+          <InputBox size="default" type="password" />
+        </label>
+        <button className=" h-[50px] bg-slate-200" type="submit">
+          acrtor project 회원으로 로그인
+        </button>
+      </form>
+      <section className="justify-around items-center w-4/6 h-[150px] flex flex-col font-bold">
+        <div className="w-full social_login_line_text text-center text-gray-400">
+          간편 로그인
+        </div>
+        <div className="flex w-full justify-evenly ">
+          <img
+            src={kakaotalkLoginButton}
+            alt="Kakaotalk Login Button"
+            className="w-[50px] h-[50px"
+          />
+          <img
+            src={naverLoginButton}
+            alt="Naver Login Button"
+            className="w-[50px] h-[50px"
+          />
+          <img
+            src={googleLoginButton}
+            alt="Google Login Button"
+            className="w-[50px] h-[50px"
+          />
+        </div>
+      </section>
+    </div>
+  );
+};
